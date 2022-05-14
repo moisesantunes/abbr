98a2a4aa604d0bb9216a47066e5dbb7f3e56fb24
tec.m.antunes@gmail.com <tec.m.antunes@gmail.com>
2022-05-14 08:56:16 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,39 @@
+let botao = document.querySelector("#botao");
+let nomeOriginal= document.querySelector("#nomeusuario")
+const botCp = document.querySelector("#botCp")
+
+const preps =["de","da",'das','do','dos']
+
+botao.addEventListener('click', (event)=>{
+  nome(nomeOriginal.value)
+})
+botCp.addEventListener("click", (event)=>{
+  copiar()
+})
+
+const nome =(val)=>{
+  let sobrenomes = val.split(" ");
+  let abrv = sobrenomes[0]
+  for (var i = 1; i < sobrenomes.length -1 ; i++) {
+    if (sobrenomes[i].length <=3) {
+      if (preps.indexOf(sobrenomes[i]) != -1) {
+        abrv+=" "+sobrenomes[i];
+      } else {
+       abrv+=" "+sobrenomes[i][0]+"." 
+      }
+    }else{
+      abrv+=" "+sobrenomes[i][0]+"."
+    }
+}
+  document.getElementById("abreviado").value= abrv.toUpperCase();
+}
+
+const copiar = ()=>{
+  
+ let txt= document.getElementById("abreviado");
+ txt.select();
+ txt.setSelectionRange(0, 70);
+ navigator.clipboard.writeText(txt.value).then(()=>{
+ alert(`O texto copiado é: ${txt.value}`)
+} )
+}
